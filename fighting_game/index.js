dd398b687b5daf9ae8f3c09d30072280d7112504
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-05 21:42:11 -0600

    Add movement using addEventListener() method

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -22,6 +22,8 @@ class Sprite {
 
     update() {
         this.draw()
+
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -67,3 +69,21 @@ function animate() {
 };
 
 animate();
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 1;
+            break;
+    };
+    console.log(event.key);
+});
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'd':
+            player.velocity.x = 0;
+            break;
+    };
+    console.log(event.key);
+});
