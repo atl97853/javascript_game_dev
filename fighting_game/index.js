df44d19baf968ddd5ec35cc1597eb7e85049f0a7
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-09 22:12:39 -0600

    Add enemy movement

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -13,6 +13,7 @@ class Sprite {
         this.position = position;
         this.velocity = velocity;
         this.height = 150;
+        this.lastKey;
     };
 
     draw() {
@@ -70,6 +71,15 @@ const keys = {
     w: {
         pressed: false
     },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    },
+    ArrowUp: {
+        pressed: false
+    },
 };
 let lastKey;
 
@@ -81,11 +91,20 @@ function animate() {
     enemy.update();
 
     player.velocity.x = 0;
+    enemy.velocity.x = 0;
 
+    // player movement
     if (keys.a.pressed && lastKey === 'a') {
         player.velocity.x = -1;
     } else if (keys.d.pressed && lastKey === 'd') {
         player.velocity.x = 1;
+    };
+
+    // enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = -1;
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = 1;
     };
 };
 
@@ -104,6 +123,19 @@ window.addEventListener('keydown', (event) => {
         case 'w':
             player.velocity.y = -10;
             break;
+
+        // enemy keys
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true;
+            enemy.lastKey = 'ArrowRight';
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true;
+            enemy.lastKey = 'ArrowLeft';
+            break;
+        case 'ArrowUp':
+            enemy.velocity.y = -10;
+            break;
     };
     console.log(event.key);
 });
@@ -118,6 +150,19 @@ window.addEventListener('keyup', (event) => {
             break;
         case 'w':
             keys.w.pressed = false;
+            break;
+    };
+
+    // enemy keys
+    switch (event.key) {
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false;
+            break;
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false;
+            break;
+        case 'ArrowUp':
+            keys.ArrowUp.pressed = false;
             break;
     };
     console.log(event.key);
