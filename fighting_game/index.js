af362b5ab67bfd387a0c54a5a2cfa953bcf51937
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-09 23:21:09 -0600

    Create attackBox property in Sprite class

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -14,6 +14,11 @@ class Sprite {
         this.velocity = velocity;
         this.height = 150;
         this.lastKey;
+        this.attackBox = {
+            position: this.position,
+            width: 100,
+            height: 50,
+        };
     };
 
     draw() {
