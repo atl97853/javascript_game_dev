a580f393ac63c06e6a310e55239ac1988e475d6d
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-10 21:59:38 -0600

    Time out functionality and fix bug

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -128,28 +128,29 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
-function determineWinner({ player, enemy }) {
-
+function determineWinner(player, enemy, timerId) {
+    clearTimeout(timerId);
+    document.querySelector('#displayText').style.display = 'flex';
+    if (player.health === enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Tie';
+    } else if (player.health > enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+    } else if (player.health < enemy.health) {
+        document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+    };
 };
 
 let timer = 60;
+let timerId;
 function decreaseTimer() {
     if (timer > 0) {
-        setTimeout(decreaseTimer, 1000);
+        timerId = setTimeout(decreaseTimer, 1000);
         timer--
         document.querySelector('#timer').innerHTML = timer;
     }
 
     if (timer === 0) {
-        document.querySelector('#displayText').style.display = 'flex';
-
-        if (player.health === enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Tie';
-        } else if (player.health > enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
-        } else if (player.health < enemy.health) {
-            document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
-        };
+        determineWinner(player, enemy, timerId);
     };
 };
 
@@ -206,8 +207,8 @@ function animate() {
 
     // end game based on health
     if (enemy.health <= 0 || player.health <= 0) {
-
-    }
+        determineWinner(player, enemy, timerId);
+    };
 };
 
 animate();
