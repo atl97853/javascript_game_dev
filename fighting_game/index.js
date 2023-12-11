b344f5d1d5582029db1e137ef3584a97d06548bd
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-10 20:59:33 -0600

    Declare function determineWinner

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -128,6 +128,33 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy }) {
+
+};
+
+let timer = 60;
+function decreaseTimer() {
+    if (timer > 0) {
+        setTimeout(decreaseTimer, 1000);
+        timer--
+        document.querySelector('#timer').innerHTML = timer;
+    }
+
+    if (timer === 0) {
+        document.querySelector('#displayText').style.display = 'flex';
+
+        if (player.health === enemy.health) {
+            document.querySelector('#displayText').innerHTML = 'Tie';
+        } else if (player.health > enemy.health) {
+            document.querySelector('#displayText').innerHTML = 'Player 1 Wins';
+        } else if (player.health < enemy.health) {
+            document.querySelector('#displayText').innerHTML = 'Player 2 Wins'
+        };
+    };
+};
+
+decreaseTimer();
+
 function animate() {
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
@@ -176,6 +203,11 @@ function animate() {
         player.health -= 20;
         document.querySelector('#playerHealth').style.width = player.health + '%';
     };
+
+    // end game based on health
+    if (enemy.health <= 0 || player.health <= 0) {
+
+    }
 };
 
 animate();
