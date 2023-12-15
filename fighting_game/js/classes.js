5d7e8d89b56714e89f7a39794d0fdb195c800987
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-14 23:52:54 -0600

    Refactor and debug Sprite class

--- fighting_game/js/classes.js
+++ fighting_game/js/classes.js
@@ -53,11 +53,14 @@ class Fighter {
     };
 };
 
-class Sprite extends Fighter {
+class Sprite {
     constructor({ position, imageSrc }) {
+        this.position = position;
+        this.width = 50;
+        this.height = 150;
         this.image = new Image();
         this.image.src = imageSrc;
-    }
+    };
 
     draw() {
         c.drawImage(this.image, this.position.x, this.position.y)
