b64e46868482974ddc3712495d31abd09a753df1
redpanda97585 <128220069+redpanda97585@users.noreply.github.com>
2023-12-15 21:56:24 -0600

    Refact draw and update methods in Fighter class

--- fighting_game/js/classes.js
+++ fighting_game/js/classes.js
@@ -79,28 +79,29 @@ class Fighter extends Sprite {
     };
 
     draw() {
-        c.fillStyle = this.color;
-        c.fillRect(this.position.x, this.position.y, this.width, this.height);
-
-        // attack box
-        if (this.isAttacking) {
-            c.fillStyle = 'green';
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height);
-        };
+        c.drawImage(
+            this.image,
+            this.frameCurrent * (this.image.width / this.framesMax),
+            0,
+            this.image.width / this.framesMax,
+            this.image.height,
+            this.position.x,
+            this.position.y,
+            (this.image.width / this.framesMax) * this.scale,
+            this.image.height * this.scale,
+        )
     };
 
     update() {
         this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
-        this.attackBox.position.y = this.position.y;
+        this.framesElapsed++
 
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
-            this.velocity.y = 0;
-        } else {
-            this.velocity.y += gravity;
+        if (this.framesElapsed % this.framesHold === 0) {
+            if (this.frameCurrent < this.framesMax - 1) {
+                this.frameCurrent++;
+            } else {
+                this.frameCurrent = 0;
+            };
         };
     };
 
