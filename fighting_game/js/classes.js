6b39531c857a2b4b1c8c4f6fb7d6f4ea98a5619a
atl97853 <128220069+atl97853@users.noreply.github.com>
2023-12-21 23:54:57 -0600

    Add sprite animation for player attack1

--- fighting_game/js/classes.js
+++ fighting_game/js/classes.js
@@ -112,6 +112,7 @@ class Fighter extends Sprite {
     };
 
     attack() {
+        this.switchSprite('attack1')
         this.isAttacking = true;
         setTimeout(() => {
             this.isAttacking = false;
@@ -119,6 +120,11 @@ class Fighter extends Sprite {
     };
 
     switchSprite(sprite) {
+        if (this.image === this.sprites.attack1.image &&
+            this.frameCurrent < this.sprites.attack1.framesMax - 1)
+            return
+
+
         switch (sprite) {
             case 'idle':
                 if (this.image !== this.sprites.idle.image) {
@@ -148,6 +154,12 @@ class Fighter extends Sprite {
                     this.frameCurrent = 0;
                 };
                 break;
+            case 'attack1':
+                if (this.image !== this.sprites.attack1.image) {
+                    this.image = this.sprites.attack1.image;
+                    this.framesMax = this.sprites.attack1.framesMax;
+                    this.frameCurrent = 0;
+                };
         };
     };
 };
