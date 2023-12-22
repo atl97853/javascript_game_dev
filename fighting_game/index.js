d59dfbacea4724e50051612cbb1b9cb9df9d7223
atl97853 <128220069+atl97853@users.noreply.github.com>
2023-12-21 23:55:15 -0600

    Add sprite animation for player attack1

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -63,6 +63,10 @@ const player = new Fighter({
             imageSrc: './assets/samuraiMack/Fall.png',
             framesMax: 2,
         },
+        attack1: {
+            imageSrc: './assets/samuraiMack/Attack1.png',
+            framesMax: 6,
+        },
     },
 });
 
