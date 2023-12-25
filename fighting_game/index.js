74b91faedfed10ec38874edb8e332e1770f584a5
atl97853 <128220069+atl97853@users.noreply.github.com>
2023-12-25 00:31:50 -0600

    Refactor code to add interface animation in the live bar

--- fighting_game/index.js
+++ fighting_game/index.js
@@ -179,6 +179,8 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height);
     background.update();
     shop.update();
+    c.fillStyle = 'rgba(255, 255, 255, 0.15)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.update();
     enemy.update();
     player.velocity.x = 0;
@@ -230,7 +232,9 @@ function animate() {
     ) {
         enemy.takeHit();
         player.isAttacking = false;
-        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
+        gsap.to('#enemyHealth', {
+            width: enemy.health + '%',
+        });
     };
 
     // if player misses
@@ -238,6 +242,7 @@ function animate() {
         player.isAttacking = false;
     };
 
+    // this is where our player gets hit
     if (
         rectangularCollision({
             rectangle1: enemy,
@@ -247,7 +252,9 @@ function animate() {
     ) {
         player.takeHit();
         enemy.isAttacking = false;
-        document.querySelector('#playerHealth').style.width = player.health + '%';
+        gsap.to('#playerHealth', {
+            width: player.health + '%',
+        });
     };
 
     // if enemy misses
