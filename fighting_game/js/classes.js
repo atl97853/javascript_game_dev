264fd437727003e796baa663615aa5d2c6299ffe
atl97853 <128220069+atl97853@users.noreply.github.com>
2023-12-24 23:44:25 -0600

    Refactor code to add Death animation and functionality

--- fighting_game/js/classes.js
+++ fighting_game/js/classes.js
@@ -85,6 +85,7 @@ class Fighter extends Sprite {
         this.framesElapsed = 0;
         this.framesHold = 5;
         this.sprites = sprites;
+        this.dead = false;
 
         for (const sprite in this.sprites) {
             sprites[sprite].image = new Image();
@@ -95,7 +96,7 @@ class Fighter extends Sprite {
 
     update() {
         this.draw();
-        this.animateFrames();
+        if (!this.dead) this.animateFrames();
 
         // attack boxes
         this.attackBox.position.x = this.position.x + this.attackBox.offset.x;
@@ -122,11 +123,22 @@ class Fighter extends Sprite {
     };
 
     takeHit() {
-        this.switchSprite('takeHit');
         this.health -= 20;
+
+        if (this.health <= 0) {
+            this.switchSprite('death');
+        } else {
+            this.switchSprite('takeHit');
+        };
     };
 
     switchSprite(sprite) {
+        if (this.image === this.sprites.death.image) {
+            if (this.frameCurrent === this.sprites.death.framesMax - 1)
+                this.dead = true;
+            return
+        };
+
         // overriding all other animations with the attack animation 
         if (this.image === this.sprites.attack1.image &&
             this.frameCurrent < this.sprites.attack1.framesMax - 1)
@@ -177,6 +189,13 @@ class Fighter extends Sprite {
                 if (this.image !== this.sprites.takeHit.image) {
                     this.image = this.sprites.takeHit.image;
                     this.framesMax = this.sprites.takeHit.framesMax;
+                    this.frameCurrent = 0;
+                };
+                break;
+            case 'death':
+                if (this.image !== this.sprites.death.image) {
+                    this.image = this.sprites.death.image;
+                    this.framesMax = this.sprites.death.framesMax;
                     this.frameCurrent = 0;
                 };
                 break;
