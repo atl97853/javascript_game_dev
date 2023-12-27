41f8561e1723c382deba70a57e8489b88edb8373
atl97853 <128220069+atl97853@users.noreply.github.com>
2023-12-26 20:21:17 -0600

    Add condition statement to define when to stop gravity effect in player instance

--- mario_game/index.js
+++ mario_game/index.js
@@ -33,8 +33,12 @@ class Player {
     update() {
         this.draw()
         this.position.y += this.velocity.y
+        if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+            this.velocity.y += gravity
+        } else {
+            this.velocity.y = 0
+        }
 
-        this.velocity.y += gravity
     }
 }
 
