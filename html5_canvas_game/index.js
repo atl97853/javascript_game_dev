0ba8d7768ea30e95f4e889c52c6abaafaf1ad950
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-01-02 19:57:33 -0600

    Add condition statement inside spawnEnemies function to make spawn of enemies more randomly

--- html5_canvas_game/index.js
+++ html5_canvas_game/index.js
@@ -9,26 +9,55 @@ const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
 const projectile = []
+const enemies = []
 
 
 function spawnEnemies() {
     setInterval(() => {
-        console.log('go')
+        const radius = 30
+        let x
+        let y
+        if (Math.random() < 0.5) {
+            x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+            y = Math.random() * canvas.height
+        } else {
+            x = Math.random() * canvas.width
+            y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+        }
+
+        const color = 'green'
+        const angle = Math.atan2(
+            canvas.height / 2 - y,
+            canvas.width / 2 - x
+        )
+
+        const velocity = {
+            x: Math.cos(angle),
+            y: Math.sin(angle)
+        }
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+        console.log(enemies)
     }, 1000)
 }
 
 function animate() {
     requestAnimationFrame(animate)
     c.clearRect(0, 0, canvas.width, canvas.height)
+
     player.draw()
+
     projectile.forEach((projectile) => {
         projectile.update()
+    })
+
+    enemies.forEach((enemy) => {
+        enemy.update()
     })
 }
 
 window.addEventListener('click', (event) => {
 
-    // projectile creation and continues
+    // set the velocity for projectiles 
     const angle = Math.atan2(
         event.clientY - y,
         event.clientX - x
@@ -49,3 +78,4 @@ window.addEventListener('click', (event) => {
 })
 
 animate()
+spawnEnemies()
