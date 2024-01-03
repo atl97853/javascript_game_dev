489d482bf68cad621cd6495ea59f7af06c77baf8
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-01-02 18:47:48 -0600

    Create spawnEnemies function and change classes from directory

--- html5_canvas_game/index.js
+++ html5_canvas_game/index.js
@@ -4,47 +4,18 @@ const c = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-class Player {
-    constructor(x, y, radius, color) {
-        this.x = x
-        this.y = y
-        this.radius = radius
-        this.color = color
-    }
-
-    draw() {
-        c.beginPath()
-        c.arc(
-            this.x,
-            this.y,
-            this.radius,
-            0,
-            Math.PI * 2,
-            false
-        )
-        c.fillStyle = this.color
-        c.fill()
-    }
-}
-
-class Projectile extends Player {
-    constructor(x, y, radius, color, velocity) {
-        super(x, y, radius, color)
-        this.velocity = velocity
-    }
-
-    update() {
-        this.draw()
-        this.x = this.x + this.velocity.x
-        this.y = this.y + this.velocity.y
-    }
-}
-
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
 const projectile = []
+
+
+function spawnEnemies() {
+    setInterval(() => {
+        console.log('go')
+    }, 1000)
+}
 
 function animate() {
     requestAnimationFrame(animate)
