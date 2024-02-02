99fa29909c2da5834dbd4232258a5142fa44933c
atl97853 <128220069+atl97853@users.noreply.github.com>
2024-02-01 23:04:36 -0600

    Change velocity movement in the x axis

--- multi_room_platformer/index.js
+++ multi_room_platformer/index.js
@@ -24,8 +24,8 @@ function animate() {
     c.fillRect(0, 0, canvas.width, canvas.height)
 
     player.velocity.x = 0
-    if (keys.d.pressed) player.velocity.x = 1
-    else if (keys.a.pressed) player.velocity.x = -1
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
 
     player.draw()
     player.update()
